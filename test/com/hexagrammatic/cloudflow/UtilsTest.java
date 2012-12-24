293f7868ef5a43c635400842454dd8940d396bc4
Bill Dimmick <me@billdimmick.com>
2012-12-24 13:05:23 -0800

    Adding unit tests for Utils.

---
+++ test/com/hexagrammatic/cloudflow/UtilsTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.hexagrammatic.cloudflow;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Bill Dimmick <me@billdimmick.com>
+ */
+public class UtilsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
